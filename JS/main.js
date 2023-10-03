a47ce60dcd2f858f3d3f61858ca86b71019e457a
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2023-10-03 22:23:39 +0100

    d

--- JS/main.js
+++ JS/main.js
@@ -14,11 +14,6 @@ cancelBtn.onclick = () => {
     menuBtn.classList.remove("hide");
 }
 
-// window.onscroll = ()=>{
-//     this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-// }
-//-----------menu bar
-
 //dark and light mode
 const icon = document.getElementById("icon");
 icon.onclick = () => {
